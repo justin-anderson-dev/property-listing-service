b7552f436c7b09652465d0d995fa7ac8aa230154
Justin Anderson <justand@gmail.com>
2020-04-21 15:53:31 -0700

    Refactor express server to work with testing environment

--- server/index.js
+++ server/index.js
@@ -7,30 +7,23 @@ const Features = require('../database/Feature.js');
 
 const pretty = require('express-prettify');
 const app = express();
-const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(pretty({ query: 'pretty' }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+// app.use(express.static(__dirname + '/../client/dist'));
 
-// GET listing
-app.get('/:id', (req, res) => {
+// GET listing data
+app.get('/listings/:id', (req, res) => {
   // res.send(`The id you specified is: ${req.params.id}`);
-  // actual behavior: get Mongo document with specified id
   Listings.findOne( {listingId: req.params.id} ).exec( (err, listing) => {
     if (err) {
       return new Error(err);
     }
     res.json(listing);
   });
-  // send index.html plus mongo doc (as state)?
 });
 
-// GET features for that listing ?? or do I send all features data along with the listing to be added to app state?
+// GET features for that listing ?? Need an endpoint for features data
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
-
-// module.exports.server = app.listen(3006);
+module.exports = app;
