6a392c99a84e386fab0dc5c145854cbf2c27b496
Justin Anderson <justand@gmail.com>
2020-04-27 16:00:24 -0700

    Add feature filter and parse amenities to simplify child props

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,13 +15,40 @@ class App extends React.Component {
     };
   }
 
+  filterFeatures(collection) {
+    // console.log(`filterFeatures invoked`);
+    var filteredList = [];
+    Object.keys(collection).forEach((key) => {
+      if (collection[key] === true) {
+        filteredList.push(key);
+      }
+    });
+    return filteredList;
+  }
+
   fetchListingData(id) {
     const self = this;
     // axios get request to /id
-    axios.get(`${API_URL}/listings/${id}`)
+    return axios.get(`${API_URL}/listings/${id}`)
       .then((listing) => {
-        console.log(`got listing data: ${Object.keys(listing.data)}`);
-        self.setState({listingData: listing.data});
+        console.log(`got listing data for listing ${id}: ${Object.keys(listing.data)}`);
+        self.setState({
+          listingData: listing.data,
+          topFeatures: self.filterFeatures(listing.data.topFeatures),
+          keyAmenities: self.filterFeatures(listing.data.keyAmenities),
+          allAmenities: {
+            basic: self.filterFeatures(listing.data.allAmenities.basic),
+            familyFeatures: self.filterFeatures(listing.data.allAmenities.familyFeatures),
+            facilities: self.filterFeatures(listing.data.allAmenities.facilities),
+            dining: self.filterFeatures(listing.data.allAmenities.dining),
+            guestAccess: self.filterFeatures(listing.data.allAmenities.guestAccess),
+            logistics: self.filterFeatures(listing.data.allAmenities.logistics),
+            bedAndBath: self.filterFeatures(listing.data.allAmenities.bedAndBath),
+            outdoor: self.filterFeatures(listing.data.allAmenities.outdoor),
+            safetyFeatures: self.filterFeatures(listing.data.allAmenities.safetyFeatures),
+            notIncluded: self.filterFeatures(listing.data.allAmenities.notIncluded)
+          }
+        });
       })
       .catch((error) => {
         return new Error(error);
@@ -32,7 +59,7 @@ class App extends React.Component {
     const self = this;
     axios.get(`${API_URL}/features/all`)
       .then((features) => {
-        console.log(`got features data for ${features.data.length} features`);
+        console.log(`got features data for all ${features.data.length} features`);
         self.setState({featuresData: features.data});
       })
       .catch((error) => {
