0e2f13b6045648d3325c102fb09cef05a2541824
Justin Anderson <justand@gmail.com>
2020-05-15 10:27:40 -0700

    Add dotenv variables to database files

--- database/Feature.js
+++ database/Feature.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const db = require('./index.js');
+const API_URL = process.env.API_URL;
 
 const featureSchema = new mongoose.Schema({
   //TODO: define features schema
   idString: {type: String, required: true},
   featureTitle: {type: String, required: true},
   featureDescr: {type: String, required: false},
-  iconUrl: {type: String, default: 'http://localhost:3005/assets/entireHome.svg'}
+  iconUrl: {type: String, default: API_URL + '/assets/entireHome.svg'}
 });
 
 const Feature = mongoose.model('Feature', featureSchema);
